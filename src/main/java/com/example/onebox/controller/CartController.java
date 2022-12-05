aafa579c4eb4af1c8e72aadbee2bf90b6225c04b
acuencalara <acuencalara@gmail.com>
2022-12-05 12:57:26 +0100

    Commit

--- src/main/java/com/example/onebox/controller/CartController.java
+++ src/main/java/com/example/onebox/controller/CartController.java
@@ -5,8 +5,10 @@ import com.example.onebox.service.CartService;
 import com.example.onebox.service.InitMasterData;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -28,14 +30,16 @@ public class CartController {
 
     @GetMapping("/cart/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Optional<Cart> getCartById(@PathVariable Long id){
-        return cartService.findById(id);
+    public ResponseEntity<Cart> getCartById(@PathVariable Long id){
+        cartService.findById(id);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/cart/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteCartById(@PathVariable Long id){
+    public ResponseEntity<Cart> deleteCartById(@PathVariable Long id){
         cartService.deleteCart(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/cart/")
